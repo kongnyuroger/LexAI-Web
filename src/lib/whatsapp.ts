7f5e6e0f0daf7765884fb0c690eeb6e9af17fc7a
Kongnyu Roger <kongnyuroger711@gmail.com>
2026-06-20 22:22:30 +0100

    updated whatsapp starte message

--- src/lib/whatsapp.ts
+++ src/lib/whatsapp.ts
@@ -1,5 +1,5 @@
 export const WHATSAPP_NUMBER = '237679901361'
 export const WHATSAPP_DISPLAY_NUMBER = '+237 679 901 361'
-export const WHATSAPP_STARTER_MESSAGE = 'Hello! I am interested in learning more about your services. Can you provide me with more information?'
+export const WHATSAPP_STARTER_MESSAGE = 'Hello! I am interested in learning more about your services?'
 
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_STARTER_MESSAGE)}`
